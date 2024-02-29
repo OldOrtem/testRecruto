7c6f7ba95ce8f7a0faefc586baf7aaf9e34a15ea
OldOrtem <artemsmirnof@mail.ru>
2024-02-29 16:13:17 +0300

    first

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 def hello():
     name = request.args.get('name')
     message = request.args.get('message')
-
-    return render_template('index.html', name=name, message=message)
+    if(name == None and message == None):
+        return render_template('form.html')
+    else:
+        return render_template('index.html', name=name, message=message)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
